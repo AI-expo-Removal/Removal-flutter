a4ed2e0217d31fa6b586ba7ae185bc295b91c964
seoyerin <us3544974@gmail.com>
2024-03-14 19:55:31 +0900

    📃 :: [#13] RichText 사용

--- lib/presentation/sign_up/widget/sign_up_app_bar.dart
+++ lib/presentation/sign_up/widget/sign_up_app_bar.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:removal_flutter/core/component/fonts/removal_text_style.dart';
-import 'package:removal_flutter/core/removal_color.dart';
+import 'package:removal_flutter/core/removal.dart';
 
 class SignUpAppBar extends StatelessWidget implements PreferredSizeWidget {
-
   const SignUpAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: PreferredSize(
-          preferredSize: preferredSize,
-          child: AppBar(
-            backgroundColor: RemovalColor.gray100,
-            title: Row(
-              children: [
-                SizedBox(width: 347),
-                Text('1',
-                  style: RemovalTextStyle.label1(
-                    color: RemovalColor.main50
-                  )
+      child: PreferredSize(
+        preferredSize: preferredSize,
+        child: AppBar(
+          backgroundColor: RemovalColor.gray100,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '1',
+                      style: RemovalTextStyle.label1(
+                        color: RemovalColor.main50,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '/2',
+                      style: RemovalTextStyle.label1(
+                        color: RemovalColor.white,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  '/2',
-                  style: RemovalTextStyle.label1(
-                      color: RemovalColor.white,
-                  ),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
-        )
+        ),
+      ),
     );
   }
+
   @override
   Size get preferredSize => const Size.fromHeight(48);
 }
