2e39c065d18b213462efe790ebc5039eceb80e3e
유지우 <treeyu12345@naver.com>
2024-03-16 21:33:34 +0900

    ➕ :: [#20] 회원가입 스크린 라우터 설정

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:removal_flutter/core/view/splash_screen.dart';
 import 'package:removal_flutter/presentation/login/view/login_screen.dart';
 import 'package:removal_flutter/presentation/on_boarding/view/on_boarding_screen.dart';
+import 'package:removal_flutter/presentation/sign_up/view/sign_up_user_info_screen.dart';
 
 final router = GoRouter(
   initialLocation: '/',
@@ -17,6 +18,10 @@ final router = GoRouter(
     GoRoute(
       path: '/login',
       builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/signUp',
+      builder: (context, state) => const SignUpScreen(),
     ),
   ],
 );
