c384a27f199ecdb62716d604d86cd49945ebaa78
유지우 <treeyu12345@naver.com>
2024-03-18 10:06:29 +0900

    🍯 :: [#20] 네이밍 변경

--- lib/presentation/sign_up/view/sign_up_user_info_screen.dart
+++ lib/presentation/sign_up/view/sign_up_user_info_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:removal_flutter/core/removal_color.dart';
-import 'package:removal_flutter/presentation/sign_up/widget/sign_up_app_bar.dart';
+import 'package:removal_flutter/presentation/sign_up/widget/user_info/sign_up_user_info_app_bar.dart';
+import 'package:removal_flutter/presentation/sign_up/widget/user_info/sign_up_user_info_action_button.dart';
+import 'package:removal_flutter/presentation/sign_up/widget/user_info/sign_up_user_info_text_field_list.dart';
+import 'package:removal_flutter/presentation/sign_up/widget/sign_up_title_widget.dart';
 
-class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+class SignUpUserInfoScreen extends StatelessWidget {
+  const SignUpUserInfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: RemovalColor.gray100,
-      appBar: const SignUpAppBar(),
+      appBar: const SignUpUserInfoAppBar(),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SignUpTitleWidget(),
+              SizedBox(height: 36),
+              SignUpUserInfoTextFieldList(),
+              Spacer(),
+              SignUpUserInfoActionButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
