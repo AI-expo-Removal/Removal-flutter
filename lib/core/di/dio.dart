fd77bc4fa60ef0538a0112c520bc309ffab56cb5
유지우 <treeyu12345@naver.com>
2024-09-12 08:38:06 +0900

    DEL :: 불필요한 impot 제거

--- lib/core/di/dio.dart
+++ lib/core/di/dio.dart
@@ -1,9 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:removal_flutter/core/di/base_url.dart';
-import 'package:removal_flutter/core/di/provider/re_issue/re_issue_view_model_provider.dart';
-import 'package:removal_flutter/core/jwt_store/jwt_store.dart';
-import 'package:removal_flutter/core/jwt_store/jwt_store_properties.dart';
 
 final Dio dio = Dio(
   BaseOptions(
