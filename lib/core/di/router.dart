db590c3933f4ea8d46d468f25a8f483da6459c32
유지우 <treeyu12345@naver.com>
2024-09-12 08:38:18 +0900

    FEAT :: GoRouter 설정 추가

---
+++ lib/core/di/router.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:go_router/go_router.dart';
+import 'package:removal_flutter/core/view/splash_screen.dart';
+import 'package:removal_flutter/presentation/main/view/main_screen.dart';
+import 'package:removal_flutter/presentation/video/view/compressed_video_screen.dart';
+import 'package:removal_flutter/presentation/video/view/video_amplify_screen.dart';
+import 'package:removal_flutter/presentation/video/view/video_screen.dart';
+import 'package:video_editor/video_editor.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: '/main',
+      builder: (context, state) => const MainScreen(),
+    ),
+    GoRoute(
+      path: '/video',
+      builder: (context, state) {
+        return VideoScreen(file: state.extra as File);
+      },
+    ),
+    GoRoute(
+      path: '/videoAmplify',
+      builder: (context, state) {
+        return VideoAmplifyScreen(controller: state.extra as VideoEditorController);
+      },
+    ),
+    GoRoute(
+      path: '/compressed',
+      builder: (context, state) {
+        return CompressedVideoScreen(path: state.extra as String);
+      },
+    ),
+  ],
+);
